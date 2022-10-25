f1e1988d6c8d46cd601c1d79a24d8c2c43313bb4
pulchnymamut <pulchnymamut@gmail.com>
2022-10-25 22:26:15 +0200

    Playing with geometry - checkpoint

--- components/Background.jsx
+++ components/Background.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber';
 import * as THREE from 'three';
-import { Stars } from '@react-three/drei';
+import { Stars, useCamera, OrbitControls } from '@react-three/drei';
 
 const Background = () => {
     return (
       <div id="canvas-container" className="h-full w-full absolute">
-        <Canvas>
+        <Canvas >
             <Suspense fallback={null}>
-                <Stars radius={300} depth={60} count={20000} factor={7} saturation={ .01 } fade={true} />
+                    <Stars radius={300} depth={60} count={20000} factor={7} saturation={.01} fade={true} />
+                    <mesh>
+                        <boxGeometry args={[2, 2, 2]} />
+                        <meshPhongMaterial />
+                    </mesh>
+                    <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} zoomSpeed={.6} panSpeed={.5} rotateSpeed={.4} />
+                    <ambientLight intensity={0.1} />
+                    <directionalLight color="red" position={[0, 0, 5]} />
             </Suspense>
         </Canvas>
       </div>
