cd0f76a8b0f1cfdf138ec5a4d91b620b1b4e8ed9
Wojtek <pulchnymamut@gmail.com>
2022-10-30 15:19:29 +0100

    moved code to seperate component

---
+++ components/TitleSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TitleSection = () => {
+  return (
+    <section id="front" className="h-screen">
+        <h1 className="text-5xl absolute top-40 left-20">
+        Wojciech Woźniak
+        </h1>
+    </section>
+  )
+}
+
+export default TitleSection
