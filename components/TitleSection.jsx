fd5c41a91fec897ff67d79f2049ac461178639e9
Wojtek <pulchnymamut@gmail.com>
2022-10-30 15:38:34 +0100

    Added new content

--- components/TitleSection.jsx
+++ components/TitleSection.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 const TitleSection = () => {
   return (
     <section id="front" className="h-screen">
-        <h1 className="text-5xl absolute top-40 left-20">
+      <div className="absolute top-40 left-20 flex flex-col gap-3">
+        <h1 className="text-5xl">
         Wojciech Woźniak
         </h1>
+        <span className="text-3xl material-symbols-outlined h-2">
+          maximize
+        </span>
+        <h2 className='text-xl'>web developer & software engineer</h2>
+      </div>
     </section>
   )
 }
