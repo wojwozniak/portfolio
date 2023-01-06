1ee70d17e091f457a87ed22a83cc1f53fea5b764
Wojtek <pulchnymamut@gmail.com>
2023-01-06 18:17:14 +0100

    Working on navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,21 +1,52 @@
 import React from 'react'
+import { useCallback, useEffect, useState, useRef } from 'react'
+import { useDetectScroll } from "@smakss/react-scroll-direction";
 
 const Nav = () => {
-    const menuItems = [
-      'start',
-      'about',
-      'skills',
-      'work',
-      'contact'
-    ];
+  const menuItems = [
+    'start',
+    'about',
+    'skills',
+    'work',
+    'contact'
+  ];
+
+  const [currentY, setScrollY] = useState(0);
+  const [scrollDir] = useDetectScroll({});
+
+  const ref = useRef<HTMLDivElement>();
+  
+  
+  const onScroll = useCallback(event => {
+    const { pageYOffset, scrollY } = window;
+    setScrollY(window.pageYOffset);
+    updateNav(pageYOffset);
+  }, [scrollDir]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    }
+  }, [scrollDir]);
+
+  function updateNav (scrollY:number) {
+    console.log(scrollY," ", scrollDir);
+    if (scrollY < window.innerHeight || scrollDir == 'down') {
+      ref.current.style.position = 'relative';
+    } else {
+      ref.current.style.position = 'fixed';
+      ref.current.style.top = '0';
+    }
+  }
   
   return (
-    <nav className="right-0 bottom-0 w-full h-[5vh] text-3xl text-right z-10 bg-red-900">
+    <nav className="w-full text-3xl text-right z-10 bg-red-900" ref={ref}>
       <ul className='flex justify-center gap-5 flex-row'>
         {
           menuItems.map((item, index) => 
           <li key={item} className="hover:cursor-pointer flex flex-row justify-end">
-            <a href={"#"+item} className="h-full flex align-center gap-2 justify-end">
+            <a href={"#"+item} className="h-full z-20 flex align-center gap-2 justify-end">
               <p className="nav__item__label" >{item}</p> 
             </a>
           </li>)
