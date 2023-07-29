1a2f1cf2647ac6ec93322d3d6e95aa13ea08e524
Wojtek <pulchnymamut@gmail.com>
2023-07-29 20:54:15 +0200

    create firs section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/nav/Navbar"
+import Start from "./components/sections/Start"
 
 function App() {
 
   return (
     <div className='App'>
         <Navbar />
+        <Start />
     </div>
   )
 }
---
+++ src/components/sections/Start.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './start.css'
+
+const Start = () => {
+  return (
+    <section className='start'>
+        Start
+    </section>
+  )
+}
+
+export default Start
