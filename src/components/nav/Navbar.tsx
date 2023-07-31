5bfa30c2969e95fa8805543cc9414d2bc2c7ec33
Wojtek <pulchnymamut@gmail.com>
2023-07-31 20:26:27 +0200

    nav fixes

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -3,11 +3,45 @@ import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 import './navbar.css'
 import LangMenu from './LangMenu';
 
+type Translations = {
+  HOME: string;
+  ABOUT: string;
+  WORKS: string;
+  CONTACT: string;
+  Resume: string;
+};
+
+
+
 const Navbar = ({ language, setLanguage }: { language: string, setLanguage: React.Dispatch<React.SetStateAction<string>> }) => {
   const [active, setActive] = useState(false);
 
   const handleClick = () => {
     setActive(!active);
+  };
+
+  const translationsList: { [key: string]: Translations } = {
+    PL: {
+      HOME: 'STRONA GŁÓWNA',
+      ABOUT: 'O MNIE',
+      WORKS: 'PROJEKTY',
+      CONTACT: 'KONTAKT',
+      Resume: 'CV',
+    },
+    EN: {
+      HOME: 'HOME',
+      ABOUT: 'ABOUT',
+      WORKS: 'WORKS',
+      CONTACT: 'CONTACT',
+      Resume: 'Resume',
+    },
+    DE: {
+      HOME: 'STARTSEITE',
+      ABOUT: 'ÜBER MICH',
+      WORKS: 'ARBEITEN',
+      CONTACT: 'KONTAKT',
+      Resume: 'Lebenslauf',
+    },
   };
 
   return (
@@ -19,27 +53,29 @@ const Navbar = ({ language, setLanguage }: { language: string, setLanguage: Reac
         <ul className={active ? 'nav__main active' : 'nav__main'}>
           <li className="nav__item">
             <a href="#home" className="nav__link">
-              HOME
+              {translationsList[language]?.HOME}
             </a>
           </li>
           <li className="nav__item">
             <a href="#about" className="nav__link">
-              ABOUT
+              {translationsList[language]?.ABOUT}
             </a>
           </li>
           <li className="nav__item">
             <a href="#works" className="nav__link">
-              WORKS
+              {translationsList[language]?.WORKS}
             </a>
           </li>
           <li className="nav__item">
             <a href="#contact" className="nav__link">
-              CONTACT
+              {translationsList[language]?.CONTACT}
             </a>
           </li>
         </ul>
         <div className='nav__right'>
-          <a className='contact__text'><p>Resume</p></a>
+          <a className='contact__text'>
+            <p>{translationsList[language]?.Resume}</p>
+          </a>
           <LangMenu language={language} setLanguage={setLanguage} />
         </div>
         <button onClick={handleClick} className={`nav__button ${active ? 'fade-in-out' : ''}`}>
