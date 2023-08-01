e5e6b687af52e06076891cb0b203dfe9aaec82b7
Wojtek <pulchnymamut@gmail.com>
2023-08-01 15:42:49 +0200

    work on start section

--- src/components/sections/start/Start.tsx
+++ src/components/sections/start/Start.tsx
@@ -4,6 +4,11 @@ import './start.css'
 const Start = () => {
   return (
     <section className='start'>
+      <div className='content'>
+        <h1 className='white'>Hey There, I'm
+          <br /><span className="lightblue"> Wojciech Woźniak</span>
+        </h1>
+      </div>
       <Avatar />
       <div className='highlight-blue' />
       <div className='highlight-green' />
