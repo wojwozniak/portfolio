cd5ccdc186df61bf38661199806ae374ce034611
Wojtek <pulchnymamut@gmail.com>
2023-08-01 15:31:02 +0200

    fixes

--- src/components/sections/Sections.tsx
+++ src/components/sections/Sections.tsx
@@ -1,10 +1,12 @@
 import Start from './start/Start'
 import './sections.css'
+import About from './about/About'
 
 const Sections = () => {
   return (
     <div className='sections'>
       <Start />
+      <About />
     </div>
   )
 }
