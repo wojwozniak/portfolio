2a74aa55c716c1b450c35ff9a4381b3638ffb0aa
Wojtek <pulchnymamut@gmail.com>
2023-08-01 16:51:13 +0200

    del

--- src/components/sections/start/Start.tsx
+++ src/components/sections/start/Start.tsx
@@ -1,16 +1,14 @@
 import Avatar from './Avatar'
 import './start.css'
-import TechSphere from './TechSphere'
 
 const Start = () => {
   return (
     <section className='start'>
       <div className='content'>
         <h1 className='white'>Hey There, I'm
-          <br /><span className="lightblue"> Wojciech Woźniak</span>
+          <br /><span className="lightblue"> Wojciech Woźniak </span>
         </h1>
       </div>
-      <TechSphere />
       <Avatar />
       <div className='highlight-blue' />
       <div className='highlight-green' />
