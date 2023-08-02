e3b803f13abacb82753961c6697a40efc6af5db3
Wojtek <pulchnymamut@gmail.com>
2023-08-02 19:29:37 +0200

    adding content

--- src/components/sections/Sections.tsx
+++ src/components/sections/Sections.tsx
@@ -1,12 +1,14 @@
 import Start from './start/Start'
 import './sections.css'
 import About from './about/About'
+import Projects from './projects/Projects'
 
 const Sections = () => {
   return (
     <div className='sections'>
       <Start />
       <About />
+      <Projects />
     </div>
   )
 }
---
+++ src/components/sections/projects/Projects.tsx
@@ -0,0 +1,11 @@
+import './projects.css'
+
+const Projects = () => {
+  return (
+    <div className='projects'>
+        Projects
+    </div>
+  )
+}
+
+export default Projects
