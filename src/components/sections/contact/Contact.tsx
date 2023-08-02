abcd48735df6278d8e166e780b0988399eba0b38
Wojtek <pulchnymamut@gmail.com>
2023-08-02 21:20:24 +0200

    restructure

--- src/components/sections/contact/Contact.tsx
+++ src/components/sections/contact/Contact.tsx
@@ -1,4 +1,3 @@
-import ContactButton from './ContactButton'
 import './contact.css'
 
 const Contact = () => {
@@ -6,8 +5,10 @@ const Contact = () => {
     <section className='contact'>
       <div className='content'>
         <h2 id="contact__title">Contact Me</h2>
-        <p id="contact__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit neque eget aliquam luctus. Morbi interdum metus.</p>  
-        <ContactButton />
+        <p id="contact__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit neque eget aliquam luctus. Morbi interdum metus.</p>
+        <button className='contact__button'>
+          Contact
+        </button>
       </div>
       <div className='big__circle' />
     </section>
