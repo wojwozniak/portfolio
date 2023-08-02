4a5cb25225a01fb84f3c1b4e115a24375a4edc14
Wojtek <pulchnymamut@gmail.com>
2023-08-02 16:41:22 +0200

    bit more refactoring

--- src/components/sections/start/Start.tsx
+++ src/components/sections/start/Start.tsx
@@ -11,8 +11,8 @@ const Start = () => {
         <p id="start__subtitle">Unlocking the potential of code through innovation and creativity.</p>
       </div>
       <Avatar />
-      <div className='highlight-blue' />
-      <div className='highlight-green' />
+      <div id="highlight--1" className='highlight-blue' />
+      <div id="highlight--2" className='highlight-green' />
     </section>
   )
 }
