9823e2ea7d4e51d0b8e9a5c91f936561eb6ae2fe
Wojtek <pulchnymamut@gmail.com>
2023-08-06 20:05:28 +0200

    another project

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -1,5 +1,6 @@
 import './projects.css'
 import Pustynna from '../../../assets/images/projects/screen-1.png'
+import Zlota from '../../../assets/images/projects/screen-2.png'
 import { useTranslation } from 'react-i18next'
 import RenderProjectCard from './RenderProjectCard';
 
@@ -11,7 +12,20 @@ const Projects = () => {
       <div className='content'>
         <h2 id="projects__title">{t('recentProjects')}</h2>
         <div className='projects__container'>
-          <RenderProjectCard title={t("survivalScoutWebsite")} description={'exampledescription'} image={Pustynna} link={'https://pustynnaburza.dabrowagornicza.zhp.pl/'} githubLink={'https://github.com/wojwozniak/wp-pustynnaburza'} />
+          <RenderProjectCard 
+            title={t("scoutWebsite")} 
+            description={'exampledescription'} 
+            image={Zlota} 
+            link={'https://zlotaosemka.dabrowagornicza.zhp.pl/'} 
+            githubLink={'https://github.com/wojwozniak/wp-zlotaosemka'} 
+          />
+          <RenderProjectCard 
+            title={t("survivalScoutWebsite")} 
+            description={'exampledescription'} 
+            image={Pustynna} 
+            link={'https://pustynnaburza.dabrowagornicza.zhp.pl/'} 
+            githubLink={'https://github.com/wojwozniak/wp-pustynnaburza'} 
+          />
         </div>
       </div>
     </section>
