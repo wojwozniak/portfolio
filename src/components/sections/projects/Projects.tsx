2a0ce025bfa371b83c12348766eff810ef897e9c
Wojtek <pulchnymamut@gmail.com>
2023-08-06 18:17:38 +0200

    1. projekt

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -1,11 +1,19 @@
 import './projects.css'
+import Pustynna from '../../../assets/images/projects/screen-1.png'
+import { useTranslation } from 'react-i18next'
+import RenderProjectCard from './RenderProjectCard';
 
 const Projects = () => {
+  const { t } = useTranslation();
+
   return (
     <section className='projects'>
-        <div className='content'>
-            <h2 id="projects__title">Recent Projects</h2>
+      <div className='content'>
+        <h2 id="projects__title">{t('recentProjects')}</h2>
+        <div className='projects__container'>
+          <RenderProjectCard title={t("survivalScoutWebsite")} description={''} image={Pustynna} link={''} githubLink={''} />
         </div>
+      </div>
     </section>
   )
 }
