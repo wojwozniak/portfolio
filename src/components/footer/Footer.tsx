aa12586242ddb4d6566d781ef8bf3d2b7d2e4af6
Wojtek <pulchnymamut@gmail.com>
2023-12-13 15:44:29 +0100

    cleaner footer code

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,24 +7,44 @@ import { IoIosMail } from "react-icons/io";
 const Footer = () => {
     const { t } = useTranslation();
 
+    const links = [
+        {
+            name: 'Email',
+            link: '',
+            icon: <IoIosMail size="32px" />
+        },
+        {
+            name: 'Twitter',
+            link: '',
+            icon: <BsTwitter size="32px" />
+        },
+        {
+            name: 'Github',
+            link: '',
+            icon: <AiFillGithub size="32px" />
+        },
+        {
+            name: 'LinkedIn',
+            link: '',
+            icon: <BsLinkedin size="32px" />
+        }
+    ]
+
     return (
         <div className='footer'>
             <div className='content'>
                 <h2 id="footer__logo">Contact</h2>
-                <p>Have any questions? Just want to say hi? Contact me!</p>
+                <p id="footer__logo--subtext">Have any questions? Just want to say hi? Contact me!</p>
                 <div className='footer__links'>
-                <a href="#" target="_blank">
-                        <IoIosMail size="32px" />
-                    </a>
-                    <a href="#" target="_blank">
-                        <BsTwitter size="32px" />
-                    </a>
-                    <a href="#" target="_blank">
-                        <AiFillGithub size="32px" />
-                    </a>
-                    <a href="#" target="_blank">
-                        <BsLinkedin size="32px" />
-                    </a>
+                    {links.map((link, index) => {
+                        return (
+                            <div key={index} className='footer__link'>
+                                <a href={link.link} target='_blank' rel='noreferrer'>
+                                    {link.icon}
+                                </a>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
             <div className='horizontal__line' />
