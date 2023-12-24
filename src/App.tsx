54d838d36d19ed6f1364dee3d044a23d1f513a88
Wojtek <pulchnymamut@gmail.com>
2023-12-24 15:20:24 +0100

    move, rename, tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer";
-import Start from './components/sections/Start';
+import Start from './components/01Start';
 import About from './components/sections/About';
 import Projects from './components/sections/Projects';
 import "./components/sections/sections.css";
---
+++ src/components/Section01Start.tsx
@@ -0,0 +1,16 @@
+const Start = () => {
+  return <section id="home"
+    className='h-screen w-screen bg-background-light flex justify-center relative'>
+    <div className='z-5 mt-[100px] font-medium w-full pl-14 pr-14'>
+      <h2 className="mt-5 text-xl font-medium">
+        Hello!
+      </h2>
+      <h1 className='color-white font-bold text-2xl'>My name is <br />
+        <span className="text-bright-blue text-4xl">Wojciech Woźniak</span>
+      </h1>
+      <p className="mt-5">I am fullstack developer Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse metus nibh, sollicitudin vel porttitor vitae, mollis eget felis. Nunc pharetra pretium justo in suscipit. Mauris tempor purus interdum, vestibulum tortor ac, efficitur ipsum. </p>
+    </div>
+  </section>
+}
+
+export default Start
