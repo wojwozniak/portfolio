8a19d92d134cea33edf0021eb5d153d2cc83205c
Wojtek <pulchnymamut@gmail.com>
2023-12-24 15:40:02 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,7 +40,6 @@ const Navbar = () => {
   const menuItems = [
     { name: 'HOME', link: '#home' },
     { name: 'ABOUT', link: '#about' },
-    { name: 'EXPERIENCE', link: '#experience' },
     { name: 'PROJECTS', link: '#projects' },
     { name: 'CONTACT', link: '#contact' },
   ]
