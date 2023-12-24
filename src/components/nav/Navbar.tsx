8453e6037e880abf6cdb658cda3367229be7059f
Wojtek <pulchnymamut@gmail.com>
2023-12-24 14:09:35 +0100

    update styling

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import { useTranslation } from 'react-i18next';
-import LangMenu from './LangMenu';
+import LangMenu from '../../ui/LangMenu';
 
 const Navbar = () => {
-  /* Translation */
   const { t } = useTranslation();
 
   /* Navbar for small screens */
@@ -49,12 +48,15 @@ const Navbar = () => {
   return (
     <nav
       style={{
-        background: prevScrollPosition !== 0 || langMenuOpen ? '#000D15' : 'transparent',
+        background:
+          prevScrollPosition !== 0 || langMenuOpen || active
+            ? '#000D15'
+            : 'transparent'
       }}
       className={
         active
           ? 'bg-background fixed top-0 left-0 h-25 w-screen transition-all z-10 flex flex-col h-full opacity-100 !pt-2.5 !pb-2.5'
-          : '!bg-red-500 h-[60px] opacity-95 fixed top-0 left-0 h-2 w-screen pl-2.5 pr-2.5 transition-all z-10 flex flex-row justify-between items-center'
+          : 'h-[60px] opacity-95 fixed top-0 left-0 h-2 w-screen pl-2.5 pr-2.5 transition-all z-10 flex flex-row justify-between items-center'
       }>
 
       <h2 className="fixed top-4 left-3">
@@ -63,11 +65,12 @@ const Navbar = () => {
 
       <ul className={
         active
-          ? '!bg-transparent md:h-auto md:overflow-visible md:flex md:flex-row md:w-4/5 md:mt-0 md:justify-center md:items-center md:gap-10 active'
+          ? '!bg-transparent md:h-auto md:overflow-visible overflow-hidden md:flex mt-14 md:flex-row md:w-4/5 md:mt-0 md:justify-center md:items-center md:gap-10 active'
           : '!bg-transparent overflow-hidden mt-25 h-0 md:h-auto md:overflow-visible md:flex md:flex-row md:w-full md:mt-0 md:justify-center md:items-center md:gap-10'
       }>
         {menuItems.map((item, index) => {
-          return <li key={index} className="nav__item">
+          return <li key={index}
+            className="border-[1px] border-t-white border-b-0 border-l-0 border-r-0 ml-3 mr-7 md:ml-0 md:mr-7">
             <a href={item.link}
               className="no-underline text-base w-full flex flex-row justify-start ml-1 items-center"
             >
