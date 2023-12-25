24329eeee5ac97d69a20667d3d690e4ba0d66320
Wojtek <pulchnymamut@gmail.com>
2023-12-25 14:10:52 +0100

    delete mucho texto

--- src/components/Section02About.tsx
+++ src/components/Section02About.tsx
@@ -1,12 +1,12 @@
 const About = () => {
   return (
-    <section id="about" className='w-screen bg-transparent z-2 relative pt-18'>
-      <div className='bg-background pt-10 w-full h-auto flex flex-col pl-10 pr-10 pb-20'>
+    <section id="about" className='w-screen bg-transparent z-2 relative pt-18 border-t-2 border-white'>
+      <div className='bg-background pt-10 w-full h-auto flex flex-col pl-14 pr-14 pb-20'>
         <h2 className="text-4xl">
           About me
         </h2>
         <p className="font-light z-5 mt-5">
-          As a passionate full stack developer, I thrive on crafting dynamic web solutions, bridging the gap between front-end aesthetics and back-end functionality, and constantly seeking new ways to elevate user experiences through clean code and cutting-edge technologies. My dedication to lifelong learning drives me to stay at the forefront of industry trends, ensuring that each project I undertake is not just a mere accomplishment but a testament to the power of innovation and collaboration. Let's embark on a journey to build transformative digital experiences together.
+          aaa
         </p>
       </div>
     </section>
