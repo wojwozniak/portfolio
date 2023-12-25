ed6da714eca51cd7beee50f3a26bb5e085bf5faa
Wojtek <pulchnymamut@gmail.com>
2023-12-25 21:13:04 +0100

    Update Overlay.tsx

--- src/ui/Overlay.tsx
+++ src/ui/Overlay.tsx
@@ -3,7 +3,9 @@ import './animation-style.css';
 const Overlay = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-background-light z-[1000000] overflow-hidden background-animation-class">
-      <div className="animation-class">Loading...</div>
+      <div className="animation-class">
+        <p className="py-4 mt-2 font-light">wojwozniak</p>
+      </div>
     </div>
   );
 };
