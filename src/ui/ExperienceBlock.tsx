caccb166aedcb048883030f2ca90fb65f1aecd21
Wojtek <pulchnymamut@gmail.com>
2023-12-25 15:57:18 +0100

    description as list

--- src/ui/ExperienceBlock.tsx
+++ src/ui/ExperienceBlock.tsx
@@ -3,22 +3,28 @@ import { experience } from '../types/experience';
 
 const ExperienceBlock: React.FC<experience> = ({ duration, title, role, description, technologies }) => {
     return (
-        <div className="flex items-start py-4 w-full h-full">
-            <div className="w-1/4 pr-4">
-                <span className="block text-right text-sm font-semibold text-gray-600">{duration}</span>
-            </div>
-            <div className="w-3/4">
-                <h3 className="text-xl font-semibold">{title}</h3>
+        <div className="flex flex-col md:flex-row items-start w-full h-full bg-background-light shadow rounded-lg p-5">
+            <div className="flex-1 w-full">
+                <div className="flex justify-between items-center w-full">
+                    <h3 className="text-xl font-semibold">{title}</h3>
+                    <span className="text-sm font-semibold text-gray-600">{duration}</span>
+                </div>
                 <h4 className="text-lg font-medium text-blue-600">{role}</h4>
-                <p className="text-gray-700 mt-2">{description}</p>
-                <div className="flex flex-wrap gap-2 mt-4">
+                <ul className="mt-2">
+                    {description.map((desc, index) => (
+                        <li key={index} className="text-gray-700">{desc}</li>
+                    ))}
+                </ul>
+            </div>
+            {technologies &&
+                <div className="w-full md:w-auto flex flex-wrap gap-2 mt-4 md:mt-0 md:ml-4">
                     {technologies.map((tech, index) => (
                         <span key={index} className="bg-gray-200 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded">
                             {tech}
                         </span>
                     ))}
                 </div>
-            </div>
+            }
         </div>
     );
 };
