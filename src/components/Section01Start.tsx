f76437a01ba4f2bf7588157c1f48dd215596ffaf
Wojtek <pulchnymamut@gmail.com>
2023-12-25 14:10:30 +0100

    visual rework for small screens

--- src/components/Section01Start.tsx
+++ src/components/Section01Start.tsx
@@ -1,14 +1,13 @@
 const Start = () => {
   return <section id="home"
     className='h-screen w-screen bg-background-light flex justify-center relative'>
-    <div className='z-5 mt-[100px] font-medium w-full pl-14 pr-14'>
-      <h2 className="mt-14 text-xl font-medium">
-        Hello!
-      </h2>
-      <h1 className='color-white font-bold text-2xl'>My name is <br />
-        <span className="text-bright-blue text-4xl">Wojciech Woźniak</span>
+    <div className='z-5 mt-[100px] font-medium w-full pl-14 pr-14 border-b-2 border-white'>
+      <h1 className='mt-14 color-white font-bold text-xl'>Hello, I am <br />
+        <span className="text-bright-blue text-5xl">Wojciech Woźniak</span>
       </h1>
-      <p className="mt-5 w-3/5">I am fullstack developer Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse metus nibh, sollicitudin vel porttitor vitae, mollis eget felis. Nunc pharetra pretium justo in suscipit. Mauris tempor purus interdum, vestibulum tortor ac, efficitur ipsum. </p>
+      <p className="mt-5 w-3/5">
+        I am software engineer building innovative and efficient solutions for the web. Currently studying Computer Science at the University of Wrocław and working on hybrid ERP systems for <a href="https://herp.pl/" target="_blank" className="text-bright-blue">HERP</a>.
+      </p>
       <button onClick={() => window.location.href = '#bg-photo'}
         className="mt-10 bg-transparent border-bright-blue border-2 text-white font-semibold py-2 px-6 rounded-sm">
         Tell me more
