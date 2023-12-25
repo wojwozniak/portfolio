273723a3840481ff4cea2619616f6671c1c8b796
Wojtek <pulchnymamut@gmail.com>
2023-12-25 19:18:21 +0100

    Add modal

--- src/ui/Project.tsx
+++ src/ui/Project.tsx
@@ -1,46 +1,47 @@
-import { FaExternalLinkAlt, FaGithub } from "react-icons/fa"
+import { FaExternalLinkAlt } from "react-icons/fa"
 import { project } from "../types/project"
-import { useTranslation } from "react-i18next"
+import { useState } from "react";
+import ProjectModal from "./ProjectModal";
 
 const Project = ({ project }: { project: project }) => {
-    const { t } = useTranslation();
+
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => {
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
+    };
 
     return (
-        <div className="relative bg-background-light text-white p-8 rounded-lg z-3 w-4/5 h-[250px] transition-all duration-300 ease-in-out border-4 border-transparent hover:border-blue-500 hover:border-4 hover:scale-[101%]">
-            <div className="bg-background z-4 opacity-1 top-0 right-0 w-full h-full absolute md:w-3/5">
-                <img
-                    src={project.image}
-                    alt="Background"
-                    className="absolute top-0 right-0 w-full h-full object-cover rounded-lg z-5 opacity-30"
-                />
-            </div>
-
-            <div className="relative z-6">
-                <h3 className="text-xl font-bold">Featured Project</h3>
-                <h4 className="text-2xl font-bold mt-4 mb-2">{project.title}</h4>
-                <p className="mb-4">{project.description}</p>
-                <div className="flex z-8">
-                    {project.link && (
-                        <a target="_blank"
-                            href={project.link}
-                            className="mr-4 flex flex-row items-center justify-center gap-2 hover:underline"
-                            rel="noopener noreferrer">
-                            <FaExternalLinkAlt />
-                            {t('visitWebsite')}
-                        </a>
-                    )}
-                    {project.githubLink && (
-                        <a target="_blank"
-                            href={project.githubLink}
-                            className="mr-4 flex flex-row items-center justify-center gap-2 hover:underline"
-                            rel="noopener noreferrer">
-                            <FaGithub />
-                            Github
-                        </a>
-                    )}
+        <>
+            <div onClick={() => openModal()}
+                className="relative bg-background-light text-white p-8 rounded-lg z-3 w-4/5 h-[250px] transition-all duration-300 ease-in-out border-4 border-transparent hover:border-blue-500 hover:border-4 hover:scale-[101%] cursor-pointer">
+                <div className="bg-background z-4 opacity-1 top-0 right-0 w-full h-full absolute md:w-3/5">
+                    <img
+                        src={project.image}
+                        alt="Background"
+                        className="absolute top-0 right-0 w-full h-full object-cover rounded-lg z-5 opacity-30 md:opacity-90"
+                    />
+                </div>
+                <div className="relative z-6 flex flex-col h-full w-full">
+                    <div>
+                        <h3 className="text-xl font-bold">Featured Project</h3>
+                        <h4 className="text-2xl font-bold mt-4 mb-2">{project.title}</h4>
+                        <p className="mb-4">{project.description}</p>
+                    </div>
+                    <span
+                        className="mr-4 flex flex-row h-full w-full flo gap-2 hover:underline items-end"
+                        rel="noopener noreferrer">
+                        <FaExternalLinkAlt size={20} />
+                        <p>Click to see more</p>
+                    </span>
                 </div>
             </div>
-        </div>
+            <ProjectModal project={project} open={modalOpen} handleClose={closeModal} />
+        </>
     )
 }
 
