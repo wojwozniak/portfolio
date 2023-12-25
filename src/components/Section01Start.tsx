903d1602fa00b757a2983980b9075422a3d6d8b1
Wojtek <pulchnymamut@gmail.com>
2023-12-25 19:56:26 +0100

    tłumaczenia sekcji start

--- src/components/Section01Start.tsx
+++ src/components/Section01Start.tsx
@@ -1,4 +1,7 @@
+import { useTranslation } from "react-i18next";
+
 const Start = () => {
+  const { t } = useTranslation();
   const scrollToSection = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     const aboutSection = document.querySelector('#about');
@@ -11,15 +14,15 @@ const Start = () => {
   return <section id="home"
     className='h-screen w-screen bg-background-light flex justify-center relative'>
     <div className='z-5 mt-[100px] font-medium w-full pl-14 pr-14'>
-      <h1 className='mt-14 color-white font-bold text-xl'>Hello, I am <br />
+      <h1 className='mt-14 color-white font-bold text-xl'>{t("WELCOME")}<br />
         <span className="text-bright-blue text-5xl">Wojciech Woźniak</span>
       </h1>
       <p className="mt-5 w-3/5">
-        I am software engineer building innovative and efficient solutions for the web. Currently studying Computer Science at the University of Wrocław and working on hybrid ERP systems for <a href="https://herp.pl/" target="_blank" className="text-bright-blue">HERP</a>.
+        {t("START_UNDER")} <a href="https://herp.pl/" target="_blank" className="text-bright-blue">HERP</a>.
       </p>
       <button onClick={(e) => scrollToSection(e)}
         className="mt-10 bg-transparent border-bright-blue border-2 text-white font-semibold py-2 px-6 rounded-sm">
-        Tell me more
+        {t("TELL_ME_MORE")}
       </button>
     </div>
   </section>
