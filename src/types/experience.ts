4eac1b5dda6204777d4fc348ea557a2ab261b94c
Wojtek <pulchnymamut@gmail.com>
2023-12-25 15:57:06 +0100

    description as points

--- src/types/experience.ts
+++ src/types/experience.ts
@@ -2,6 +2,6 @@ export interface experience {
     duration: string;
     title: string;
     role: string;
-    description: string;
-    technologies: string[];
+    description: string[];
+    technologies?: string[];
 }
