2207da43e6cd7007c62f2f29900d6e753539cf8a
Wojtek <pulchnymamut@gmail.com>
2023-12-25 20:05:27 +0100

    a bit of translations

--- src/components/Section02About.tsx
+++ src/components/Section02About.tsx
@@ -3,27 +3,14 @@ import { useTranslation } from "react-i18next";
 const About = () => {
   const { t } = useTranslation();
 
-  const texts = [
-    "Hello! I am Wojciech Woźniak. I am a software engineer specializing in web development.",
-    "My professional journey started few years back, when I first stumbled upon basic HTML&CSS course. Soon after I started building my first websites.",
-    "Since then I started studying Computer Science and have worked on many projects, both commercial and not.",
-    "In free time I enjoy playing chess and hiking."
-  ]
-
-  const favTechs = [
-    "React",
-    "TypeScript",
-    "Next.js",
-    "Node.js",
-    "C#",
-    "Blazor"
-  ]
+  const texts = [ t("ABOUT_1"), t("ABOUT_2"), t("ABOUT_3"), t("ABOUT_4")];
+  const favTechs = [ "React", "TypeScript", "Next.js", "Node.js", "C#", "Blazor"];
 
   return (
     <section id="about" className='w-screen bg-transparent z-2 relative pt-18'>
       <div className='bg-background-light pt-10 w-full h-auto flex flex-col pl-14 pr-14 pb-20'>
         <h2 className="text-4xl">
-          About me
+          {t("ABOUT").charAt(0) + t("ABOUT").slice(1).toLowerCase()}
         </h2>
         {texts.map((text, index) => (
           <p key={index} className="font-light z-5 mt-5">
@@ -32,7 +19,7 @@ const About = () => {
         )
         )}
         <h3 className="z-5 mt-10 font-semibold text-xl">
-          Technologies I work most often with:
+          {t("FAV_TECHS")}
         </h3>
         <ul className="grid grid-cols-3 gap-4 mt-5">
           {favTechs.map((tech, index) => {
