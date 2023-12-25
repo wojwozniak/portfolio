51a08c413c58740741b561a4db321bf93f24d1a6
Wojtek <pulchnymamut@gmail.com>
2023-12-25 14:52:06 +0100

    Experience section with content added

--- src/components/Section03Experience.tsx
+++ src/components/Section03Experience.tsx
@@ -1,19 +1,44 @@
-import ExperienceBlock from "../ui/ExperienceBlock"
+import { experience } from "../types/experience"
+import TabComponent from "../ui/TabComponent"
 
 const Section03Experience = () => {
-  return (
-    <section id="about" className='w-screen bg-transparent z-2 relative pt-18'>
-      <div className='bg-background pt-10 w-full h-auto flex flex-col pl-14 pr-14 pb-20'>
-        <h2 className="text-4xl">
-          About me
-        </h2>
-        <p className="font-light z-5 mt-5">
-          aaa
-        </p>
-        <ExperienceBlock description="abcd" duration="2022-2023" role="role" technologies={["react", 'typescript']} title="title" />
-      </div>
-    </section>
-  )
+    const experience: experience[] = [
+        {
+            title: 'HERP',
+            duration: '07.2023-now',
+            role: 'Fullstack developer',
+            description: '',
+            technologies: ["react", 'typescript']
+        },
+        {
+            title: 'title',
+            duration: '2022-2023',
+            role: 'role',
+            description: 'abcd',
+            technologies: ["react", 'typescript']
+        },
+        {
+            title: 'title',
+            duration: '2022-2023',
+            role: 'role',
+            description: 'abcd',
+            technologies: ["react", 'typescript']
+        }
+    ]
+
+    return (
+        <section id="about" className='w-screen bg-transparent z-2 relative pt-18'>
+            <div className='bg-background pt-10 w-full h-auto flex flex-col pl-14 pr-14 pb-20'>
+                <h2 className="text-4xl">
+                    Experience
+                </h2>
+                <p className="font-light z-5 mt-5">
+                    Some of the places I have worked or studied at:
+                </p>
+                <TabComponent Experience={experience} />
+            </div>
+        </section>
+    )
 }
 
 export default Section03Experience
